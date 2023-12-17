4f062a39ae4e1169aaab26f8e8c6bc8d5b1287d1
shinhee-lee <oky0706@gmail.com>
2023-12-18 00:51:24 +0900

    add start script

--- app.js
+++ app.js
@@ -205,7 +205,8 @@ app.use((err, req, res, next) => {
 });
 //****************************************************************************************
 
+const port = process.env.PORT || 3000;
 //PORT
-app.listen(3000, () => {
-  console.log("Serving on port 3000");
+app.listen(port, () => {
+  console.log(`Serving on port ${port}`);
 });
